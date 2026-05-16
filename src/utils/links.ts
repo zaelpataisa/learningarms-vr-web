b78f2ed6536bac9669564b5ff16609479367a3d1
Zael Aisa <zaelaisa_micronoft@hotmail.com>
2026-05-16 16:38:20 -0400

    Embed youtube link

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -20,7 +20,7 @@ export const external_links = (link_name: string) => {
       break;
 
     case "video_demo_play":
-      return "https://youtube.com/shorts/uS6cHjPppiw?feature=share"
+      return "https://youtube.com/embed/uS6cHjPppiw"
       break;
 
     case "sidequest_link":
